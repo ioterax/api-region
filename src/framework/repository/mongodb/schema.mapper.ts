2ec4579588ada7f7844a754783dfe3a7d0696723
ferreiraad <es.adrianof@gmail.com>
2024-09-07 18:09:39 +0200

    fix steta mapper

---
+++ src/framework/repository/mongodb/schema.mapper.ts
@@ -0,0 +1,29 @@
+import { CountryOutPort } from "@/application/ports/out/country.out.port";
+import { CountryMongoDbRepository } from "./country.repository";
+import { Country, CountrySchema } from "./schemas/country.schema";
+import { State, StateSchema } from "./schemas/state.schema";
+import { StateOutPort } from "@/application/ports/out/state.out.port";
+import { StateMongoRepository } from "./state.repository";
+import { DatabaseConfigOptions } from "@/modules/context.module";
+
+export const regionConfig: DatabaseConfigOptions = {  
+  connectName: process.env.MONGO_REGION_CN_NAME as string,
+  dbName: 'region',
+  models: [
+    { name: Country.name, schema: CountrySchema },
+  ],
+  outPortProviders: [
+    { provide: CountryOutPort, useClass: CountryMongoDbRepository },
+  ]
+}
+
+export const stateConfig: DatabaseConfigOptions = {
+  connectName: 'testConnection',
+  dbName: 'testDb',
+  models: [
+    { name: State.name, schema: StateSchema }
+  ],
+  outPortProviders: [
+    { provide: StateOutPort, useClass: StateMongoRepository },
+  ]
+}
